785e37ef42ff366dba3ebdaa697d683670454c9e
roshini9825-source <228519416+roshini9825-source@users.noreply.github.com>
2026-09-12 16:15:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ def train_models(data):
 
         model = RandomForestRegressor(
 
-            n_estimators=300,
+            n_estimators=100,
 
             max_depth=None,
 
@@ -324,7 +324,7 @@ def train_models(data):
 
             random_state=42,
 
-            n_jobs=-1
+            n_jobs=2
 
         )
 
