6191809c16d8cdc349e934ce35c2a300953fe0e0
roshini9825-source <228519416+roshini9825-source@users.noreply.github.com>
2026-09-12 16:43:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -311,7 +311,7 @@ def train_models(data):
         if disease_name == "Hepatitis A":
 
              model = ExtraTreesRegressor(
-                n_estimators=300,
+                n_estimators=100,
                 max_depth=None,
                 min_samples_split=2,
                 min_samples_leaf=1,
